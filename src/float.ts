5797c2a5e31d22619d7f10521b56b37fdf787e1b
RobDWaller <rdwaller1984@gmail.com>
2020-06-22 09:58:06 +0100

    Added ceiling method to float module along with happy path tests.

--- src/float.ts
+++ src/float.ts
@@ -9,10 +9,22 @@ function fixedFloor(float: number, decimals: number): number {
   return parseFloat((Math.floor(float * multiplier(decimals))/multiplier(decimals)).toFixed(decimals))
 }
 
+function fixedCeiling(float: number, decimals: number): number {
+  return parseFloat((Math.ceil(float * multiplier(decimals))/multiplier(decimals)).toFixed(decimals))
+}
+
 export function floor(actual: number, expected: number, decimals: number): Result<string> {
   if (fixedFloor(actual, decimals) === fixedFloor(expected, decimals)) {
     return ok(`${fixedFloor(actual, decimals)} is equal to ${fixedFloor(expected, decimals)}`);
   }
 
   return err(error(fixedFloor(actual, decimals), fixedFloor(expected, decimals), "is not equal to"));
+}
+
+export function ceiling(actual: number, expected: number, decimals: number): Result<string> {
+  if (fixedCeiling(actual, decimals) === fixedCeiling(expected, decimals)) {
+    return ok(`${fixedCeiling(actual, decimals)} is equal to ${fixedCeiling(expected, decimals)}`);
+  }
+
+  return err(error(fixedCeiling(actual, decimals), fixedCeiling(expected, decimals), "is not equal to"));
 }
--- tests/float.test.ts
+++ tests/float.test.ts
@@ -2,12 +2,21 @@ import { Result } from "https://deno.land/x/resulty/mod.ts";
 import { assertTrue } from "../mod.ts";
 import {
   floor,
+  ceiling,
 } from "../src/float.ts";
 
 Deno.test("Float Floor", () => {
   assertTrue(floor(1.472, 1.475, 2).isOk());
 });
 
-Deno.test("Float Floor", () => {
+Deno.test("Float Floor Three Decimals", () => {
   assertTrue(floor(0.4728, 0.4726, 3).isOk());
+});
+
+Deno.test("Float Ceiling", () => {
+  assertTrue(ceiling(2.6723, 2.6729, 3).isOk());
+});
+
+Deno.test("Float Ceiling Zero Decimals", () => {
+  assertTrue(ceiling(0.4, 0.8, 0).isOk());
 });
