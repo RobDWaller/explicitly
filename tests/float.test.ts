ef62cad7f4ad5fc6ed6f0728f13578222810c29a
RobDWaller <rdwaller1984@gmail.com>
2020-06-22 10:03:01 +0100

    Added some failing tests to float tests module.

--- tests/float.test.ts
+++ tests/float.test.ts
@@ -1,5 +1,5 @@
 import { Result } from "https://deno.land/x/resulty/mod.ts";
-import { assertTrue } from "../mod.ts";
+import { assertTrue, assertFalse } from "../mod.ts";
 import {
   floor,
   ceiling,
@@ -13,10 +13,18 @@ Deno.test("Float Floor Three Decimals", () => {
   assertTrue(floor(0.4728, 0.4726, 3).isOk());
 });
 
+Deno.test("Float Floor Three Decimals Fail", () => {
+  assertFalse(floor(0.4732, 0.4726, 3).isOk());
+});
+
 Deno.test("Float Ceiling", () => {
   assertTrue(ceiling(2.6723, 2.6729, 3).isOk());
 });
 
 Deno.test("Float Ceiling Zero Decimals", () => {
   assertTrue(ceiling(0.4, 0.8, 0).isOk());
+});
+
+Deno.test("Float Ceiling Zero Decimals Fail", () => {
+  assertFalse(ceiling(1.4, 0.8, 0).isOk());
 });
