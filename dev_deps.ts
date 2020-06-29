dbd354d84237a990235ddac094c0d82afd99a469
RobDWaller <rdwaller1984@gmail.com>
2020-06-29 16:07:43 +0100

    Upgraded resulty dependency to v 0.3.1.

--- dev_deps.ts
+++ dev_deps.ts
@@ -6,4 +6,4 @@ export {
   red,
   green,
 } from "https://deno.land/std@0.59.0/fmt/colors.ts";
-export { Result } from "https://deno.land/x/resulty@0.2.0/mod.ts";
+export { Result } from "https://deno.land/x/resulty@0.3.1/mod.ts";
